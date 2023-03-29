f94013f43cde176ae0114413275d41543ac1b5da
ThXilos <theodksylo@gmail.com>
2023-03-29 21:29:56 +0300

    fix

--- server.js
+++ server.js
@@ -94,12 +94,12 @@ app.post("/api/sendemail", async (req, res) => {
   <body style="background-color:#f7f7f7; font-family:Arial, sans-serif; font-size:16px; line-height:1.5; color:#333333;">
     <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px; margin:0 auto;">
       <tr>
-        <td align="center" bgcolor="#ffffff" style="border-radius:5px; box-shadow:0px 2px 5px rgba(0,0,0,0.1); padding:20px;">
-          <h1 style="font-size:28px; margin:0;">Tinos Miles Car Rental Service</h1>
+        <td align="center" bgcolor="#6ac1b7" style="border-radius:5px; box-shadow:0px 2px 5px rgba(0,0,0,0.1); padding:20px;">
+          <h1 style="font-size:28px; margin:0;color:#fff;">Tinos Miles Car Rental Service</h1>
         </td>
       </tr>
       <tr>
-        <td align="left" bgcolor="#ffffff" style="border-radius:5px; box-shadow:0px 2px 5px rgba(0,0,0,0.1); padding:50px 50px 25px;">
+        <td align="left" bgcolor="#ffffff" style="border-radius:5px; box-shadow:0px 2px 5px rgba(0,0,0,0.1); padding:50px 10px 25px;">
           <p style="margin:0 0 20px 0;color:rgb(10, 150, 149);font-weight:600; font-size:24px;">Hi, ${name}</p>
           <p style="margin:0 0 20px 0;">Thank you for contacting Tinos Miles with the following inquiry:</p>
           <p style="margin:0 0 10px 0;">Pick-up <strong>${pickupDate}</strong> at <strong>${pickupTime}</strong></p>
@@ -107,22 +107,36 @@ app.post("/api/sendemail", async (req, res) => {
           <p style="margin:0 0 20px 0;">Location <strong>${pickupLocation}</strong></p>
           <p style="margin:0;">Total days: <strong>${rentDays}</strong></p>
           <p style="margin:20px 0 0;">We will get back to you as soon as possible.</p>
-          <p style="margin:40px 0 0;color:rgb(10, 150, 149);font-weight:600;">In the meantime&nbsp;click&nbsp;the button below to check out&nbsp;a&nbsp;our guide from Tinos&nbsp;with our tips&nbsp;on what to see and where to go.</p>
+          <p style="margin:20px 0 0;color:rgb(10, 150, 149);font-weight:600;letter-spacing:1.1px;text-align:center;">In the meantime&nbsp;click&nbsp;the button below to check out&nbsp;a&nbsp;our guide from Tinos&nbsp;with our tips&nbsp;on what to see and where to go.</p>
           <a href="https://tinosmiles.gr/tinos-island-guide" target="_blank" style="text-decoration:none;">
           <p style="margin:20px auto;text-align:center;background-color:rgb(230, 126, 34);max-width:120px;
                     padding:12px 24px;border-radius:9px;color:#fff">Go to Guide</p>
           </a>
           <p style="margin:40px auto 0;text-align:center;">Have a&nbsp;question? <a href="mailto:contact@tinosmiles.gr" target="_blank" style="color: #127DB3; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 160%;">contact@tinosmiles.gr</a></p>
         </td>
-      </tr>
+    </table>
+      <table align="center" border="0" cellpadding="0" cellspacing="0" width="60%" style="max-width:600px; margin:20px auto 0;">
       <tr>
-        <td align="center" bgcolor="#ffffff" style="border-radius:5px; box-shadow:0px 2px 5px rgba(0,0,0,0.1); padding:20px;">
-          <p style="margin:0; font-size:14px; color:#777777;">&copy; Tinos Miles 2023</p>
-          <p style="margin:0; font-size:14px; color:#777777;">&nbsp;Car Rental Services &nbsp;|&nbsp;Ag.markos,     Tinos 84200<br/>GNTO Reg. No:1178E81000946601&nbsp;|&nbsp;
-      <a href="tel:+30 6944187668" target="_blank" style="text-decoration: none;color:#777777;">Tel:+30 694 4187 668</a></p>
-          
+        <td style="width:50%; vertical-align:top; padding:10px;text-align:center;">
+          <!-- Left column content goes here -->
+          <a href="https://www.instagram.com/tinosmiles/" target="_blank">
+           <img style="max-width:50px;" src="https://res.cloudinary.com/dtekdjcan/image/upload/v1677243765/tinosmiles.gr/instagram_ccav7w.png"/> 
+          </a>
         </td>
-        
+        <td style="width:50%; vertical-align:top; padding:10px;text-align:center;">
+          <!-- Right column content goes here -->
+          <a href="https://www.facebook.com/tinossmiles/" target="_blank">
+           <img style="max-width:50px;" src="https://res.cloudinary.com/dtekdjcan/image/upload/v1677243765/tinosmiles.gr/facebook_b5b6hr.png"/>   
+          </a>
+        </td>
+      </tr>
+    </table>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px; margin:0 auto;font-size:12px;">
+      <tr>  
+        <td align="center" style="border-radius:5px; padding:10px;">
+          <p style="margin:0; color:#777777;">&nbsp;&copy; Tinos Miles 2023&nbsp;|&nbsp;Car Rental Services &nbsp;|&nbsp;Ag.markos,     Tinos 84200<br/>GNTO Reg. No:1178E81000946601&nbsp;|&nbsp;
+          <a href="tel:+30 6944187668" target="_blank" style="text-decoration: none;color:#777777;">Tel:+30 694 4187 668</a></p>
+        </td>
       </tr>
     </table>
   </body>
